a949de423ffddc59912d2f2f9a46be02e2d764b6
Paige <pnkrsnak@gmail.com>
2016-05-06 14:24:29 -0400

    index sends

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
+app.set('views', __dirname + '/public/views');
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-	res.send('Hello World!');
+	res.sendFile(__dirname + '/public/views/index.html');
 });
 
 app.listen(3000, function () {
